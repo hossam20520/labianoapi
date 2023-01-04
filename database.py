2dc980fb269cf98ad714011d82628547b62d3561
hossam20520 <hossamhassan889@gmail.com>
2023-01-04 15:49:54 +0200

    req

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker , scoped_session
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 #SQLALCHEMY_DATABASE_URL = "mariadb+mariadbconnector://root:@localhost:3306/fastapi"
-SQLALCHEMY_DATABASE_URL ="mysql://cskt3ep99hq9r9w3:pohquqlhu0lhuqbz@iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mtw0sc05n360kuww"
+SQLALCHEMY_DATABASE_URL ="mysql+pymysql://cskt3ep99hq9r9w3:pohquqlhu0lhuqbz@iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mtw0sc05n360kuww"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
